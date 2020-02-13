75d090b42fcf3870765e104e6f4b6087b21fe1b8
maxwellmiquelino <61025078+maxwellmiquelino@users.noreply.github.com>
2020-02-13 19:46:46 -0300

    conexaoSQLite

---
+++ conexaoSQLite/conexaoSQLite/frmConexaoSQLite.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace conexaoSQLite
+{
+    partial class frmConexaoSQLite
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(472, 160);
+            this.label1.TabIndex = 0;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 178);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Conexão com SQLite";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ conexaoSQLite/conexaoSQLite/frmConexaoSQLite.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace conexaoSQLite
+{
+    public partial class frmConexaoSQLite : Form
+    {
+        public frmConexaoSQLite()
+        {
+            InitializeComponent();
+        }
+
+        private string strConexao = String.Format(@"Data Source={0}\{1}",
+                                                   Application.StartupPath,
+                                                   "cnxSQLite.db;");
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                using(SQLiteConnection cnx = new SQLiteConnection(strConexao))
+                {
+                    if (cnx.State == ConnectionState.Closed)
+                    {
+                        cnx.Open();
+                        label1.Text = "Conexão com SQLite efetuado com sucesso !!!";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
